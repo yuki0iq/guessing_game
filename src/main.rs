2f400237cd2c4e8f3e22f44a9f858c5f1c11b0c5
Yuki <yuki.utk8g@gmail.com>
2023-06-23 16:31:19 +0300

    add random generator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use rand::Rng;
 use std::io;
 
 fn get_guess() -> String {
     println!("Guess the number");
-    
+
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin()
@@ -15,7 +16,9 @@ fn get_guess() -> String {
 fn main() {
     println!("Hello, world!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("-> secret is {secret_number}");
+
     let current_guess = get_guess();
     println!("your guess is {current_guess}");
 }
-
