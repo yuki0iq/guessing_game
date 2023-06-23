7b7a2edb1b09d242949c80730826498c1405fd50
Yuki <yuki.utk8g@gmail.com>
2023-06-23 20:40:16 +0300

    replace error-panic with error-handling

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn get_guess(count: i32) -> i32 {
+fn get_guess(count: i32) -> Result<i32, ()> {
     println!("Please input your {count}-th guess.");
     let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    guess.trim().parse().expect("Please type a number!")
+    match io::stdin().read_line(&mut guess) {
+        Ok(_) => match guess.trim().parse() {
+            Ok(result) => Ok(result),
+            Err(_) => Err(()),
+        },
+        Err(_) => Err(()),
+    }
 }
 
 fn main() {
@@ -18,23 +20,28 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("-> secret is {secret_number}");
 
-    let mut count = 0;
+    let mut count = 1;
     loop {
-        count += 1;
-        let current_guess = get_guess(count);
-        let res = current_guess.cmp(&secret_number);
+        let guess = get_guess(count);
+        match guess {
+            Ok(current_guess) => {
+                let res = current_guess.cmp(&secret_number);
 
-        println!(
-            "your {count}-th guess is {current_guess}, {}",
-            match res {
-                Ordering::Less => "too low",
-                Ordering::Equal => "you win",
-                Ordering::Greater => "too high",
+                println!(
+                    "your {count}-th guess is {current_guess}, {}",
+                    match res {
+                        Ordering::Less => "too low",
+                        Ordering::Equal => "you win",
+                        Ordering::Greater => "too high",
+                    }
+                );
+
+                if res == Ordering::Equal {
+                    break;
+                }
             }
-        );
-
-        if res == Ordering::Equal {
-            break
+            Err(_) => continue,
         }
+        count += 1;
     }
 }
