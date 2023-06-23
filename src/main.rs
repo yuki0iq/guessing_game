cdd04e2dfb4c9a2b058f6236a97ab85c1086382b
Yuki <yuki.utk8g@gmail.com>
2023-06-23 14:07:55 +0300

    2.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    println!("Проверка юникода.");
+
+    println!("Guess the number");
+    
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guess is {guess}");
 }
+
